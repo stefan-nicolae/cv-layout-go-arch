23b3999b774baad99e625a08f235a88be6c8eff7
stef11111 <84786481+stef11111@users.noreply.github.com>
2021-07-08 18:22:47 +0300

    js

---
+++ resources/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let x = document.getElementsByClassName("dropdown")[0];
+let menu = document.querySelector("menu");
+
+
+
+x.addEventListener("click", function() {
+    let w = window.innerWidth;
+    if(w < 900) {
+        menu.style.display = "flex"; //important
+    }
+});
+
+
+document.querySelector("#close").addEventListener("click", function() {
+    setTimeout(function(){ 
+        menu.style.display = "none";
+    }, 100);
+});
